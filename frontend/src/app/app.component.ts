e33114337f6a2811fc7a93a8dc0a98b9071f8e9c
vitalii.skliarov <vitalii.skliarov@chisw.com>
2024-01-19 18:46:10 +0200

    added component ts and html

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {EmployeeOutDto} from "./employee";
+import {EmployeeService} from "./employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit {
+  public employees: EmployeeOutDto[];
+
+  public constructor(private employeeService: EmployeeService) {
+    this.employees = [];
+  }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: EmployeeOutDto[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
+  }
 }
