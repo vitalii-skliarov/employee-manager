dbd91e5b4d6356cb572e229d08cc3efd2301f3d2
vitalii.skliarov <vitalii.skliarov@chisw.com>
2024-01-19 18:46:34 +0200

    added module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,8 +6,6 @@ import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
