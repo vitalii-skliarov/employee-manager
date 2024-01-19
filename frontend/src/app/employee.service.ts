37bc5eb11e03042eccba96bd4f2f2cd4f6a1cccd
vitalii.skliarov <vitalii.skliarov@chisw.com>
2024-01-19 18:37:39 +0200

    added base url

--- frontend/src/app/employee.service.ts
+++ frontend/src/app/employee.service.ts
@@ -2,12 +2,13 @@ import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {EmployeeAddDto, EmployeeOutDto, EmployeeUpdateDto} from "./employee";
+import {environment} from "../envorinment/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {
   }
