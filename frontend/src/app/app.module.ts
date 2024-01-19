d6844c757b5434ba797143c1c2f87fdd24e151f2
vitalii.skliarov <vitalii.skliarov@chisw.com>
2024-01-19 19:37:33 +0200

    fixed issue

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { HttpClientModule} from "@angular/common/http";
 import {NgModule} from "@angular/core";
 import {EmployeeService} from "./employee.service";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [],
-  exports: [],
+  exports: [CommonModule],
   imports: [HttpClientModule],
   providers: [EmployeeService],
   bootstrap: []
