7ee321230cda79ee8a1228c5a340e5e39f2d9da7
vitalii.skliarov <vitalii.skliarov@chisw.com>
2024-01-23 00:58:05 +0200

    changed data type

--- frontend/src/app/employee.service.ts
+++ frontend/src/app/employee.service.ts
@@ -1,8 +1,8 @@
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {EmployeeAddDto, EmployeeOutDto, EmployeeUpdateDto} from "./employee";
 import {environment} from "../envorinment/environment";
+import {Employee} from "./employee";
 
 @Injectable({
   providedIn: 'root'
@@ -13,23 +13,23 @@ export class EmployeeService {
   constructor(private http: HttpClient) {
   }
 
-  public getEmployees(): Observable<EmployeeOutDto[]> {
-    return this.http.get<EmployeeOutDto[]>(`${this.apiServerUrl}/api/employees`)
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/api/employees`)
   }
 
-  public getByIdEmployees(employeeId: number): Observable<EmployeeOutDto> {
-    return this.http.get<EmployeeOutDto>(`${this.apiServerUrl}/api/employees/${employeeId}`)
+  public getByIdEmployees(employeeId: number): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiServerUrl}/api/employees/${employeeId}`)
   }
 
-  public addEmployee(employee: EmployeeAddDto): Observable<EmployeeOutDto> {
-    return this.http.post<EmployeeOutDto>(`${this.apiServerUrl}/api/employees`, employee)
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/api/employees`, employee)
   }
 
-  public updateEmployee(employeeId: number, employee: EmployeeUpdateDto): Observable<EmployeeOutDto> {
-    return this.http.put<EmployeeOutDto>(`${this.apiServerUrl}/api/employees/{employeeId}`, employee)
+  public updateEmployee(employeeId: number, employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/api/employees/${employeeId}`, employee)
   }
 
-  public deleteEmployee(employeeId: number): void {
-    this.http.delete<number>(`${this.apiServerUrl}/api/employees/${employeeId}`)
+  public deleteEmployee(employeeId?: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/api/employees/${employeeId}`)
   }
 }
--- frontend/src/app/employee.ts
+++ frontend/src/app/employee.ts
@@ -1,24 +1,9 @@
-export interface EmployeeOutDto {
+export interface Employee {
   id: number;
   name: string;
   email: string;
   jobTitle: string;
   phoneNumber: string;
   imageUrl: string;
-}
-
-export interface EmployeeAddDto {
-  name: string
-  email: string
-  jobTitle: string
-  phoneNumber: string
-  imageUrl: string
-}
-
-export interface EmployeeUpdateDto {
-  name: string
-  email: string
-  jobTitle: string
-  phoneNumber: string
-  imageUrl: string
+  employeeCode: string;
 }
